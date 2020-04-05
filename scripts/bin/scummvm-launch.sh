55974c1c6d632b99150dc61c6f38ccffd9dac41c
Lothar Serra Mari <mail@serra.me>
2020-04-05 15:16:50 +0200

    Hook up speech-dispatcher for TTS support

--- scripts/bin/scummvm-launch.sh
+++ scripts/bin/scummvm-launch.sh
@@ -10,6 +10,11 @@ then
   fi
 fi
 
+# Hook up speech-dispatcher
+mkdir -p $XDG_RUNTIME_DIR/speech-dispatcher
+$SNAP/usr/bin/speech-dispatcher -d -C "$SNAP/etc/speech-dispatcher" -S "$XDG_RUNTIME_DIR/speech-dispatcher/speechd.sock" -m "$SNAP/usr/lib/speech-dispatcher-modules" -l 5
+echo "started speech-dispatcher!"
+
 # We need to do this for the user that launches scummvm, so
 # it can't be done on installation
 while [ ! -f "${XDG_CONFIG_HOME}/scummvm/scummvm.ini" ]
