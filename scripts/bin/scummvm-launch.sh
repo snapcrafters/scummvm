c6b579c878a31abe16ef0059c5638188d96cdaf1
Lothar Serra Mari <mail@serra.me>
2020-04-15 08:13:22 +0200

    Increase timeout for speech-dispatcher to 30 seconds

--- scripts/bin/scummvm-launch.sh
+++ scripts/bin/scummvm-launch.sh
@@ -12,7 +12,7 @@ fi
 
 # Hook up speech-dispatcher
 mkdir -p $XDG_RUNTIME_DIR/speech-dispatcher
-$SNAP/usr/bin/speech-dispatcher -d -C "$SNAP/etc/speech-dispatcher" -S "$XDG_RUNTIME_DIR/speech-dispatcher/speechd.sock" -m "$SNAP/usr/lib/speech-dispatcher-modules"
+$SNAP/usr/bin/speech-dispatcher -d -C "$SNAP/etc/speech-dispatcher" -S "$XDG_RUNTIME_DIR/speech-dispatcher/speechd.sock" -m "$SNAP/usr/lib/speech-dispatcher-modules" -t 30
 
 # We need to do this for the user that launches scummvm, so
 # it can't be done on installation
