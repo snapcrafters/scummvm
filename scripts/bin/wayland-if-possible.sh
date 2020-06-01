b52fbfaed1b984b1409531d57521f2a93a8a9ea0
Alan Griffiths <alan@octopull.co.uk>
2020-06-01 20:01:56 +0100

    Check correct Wayland socket (and that the interface is connected) (#25)
    * Check correct Wayland socket (and that the interface is connected)

--- scripts/bin/wayland-if-possible.sh
+++ scripts/bin/wayland-if-possible.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ -O "${XDG_RUNTIME_DIR}/wayland-0" ]; then export SDL_VIDEODRIVER=wayland; fi
+if [ -O "$XDG_RUNTIME_DIR/${WAYLAND_DISPLAY:-wayland-0}" ] && [ -e $SNAP_COMMON/wayland.connected ]; then export SDL_VIDEODRIVER=wayland; fi
 
 exec "$@"
