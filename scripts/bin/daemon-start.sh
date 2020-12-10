57dc77e602ca8c3946012b527ac80f7f2c71284a
Alan Griffiths <alan@octopull.co.uk>
2020-12-10 10:51:11 +0000

    Improved daemon-start.sh script

--- scripts/bin/daemon-start.sh
+++ scripts/bin/daemon-start.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-mkdir -p "$XDG_RUNTIME_DIR" -m 700
+real_xdg_runtime_dir=$(dirname "${XDG_RUNTIME_DIR}")
+real_wayland=${real_xdg_runtime_dir}/${WAYLAND_DISPLAY:-wayland-0}
 
-if [ -z "${WAYLAND_DISPLAY}" ]
-then WAYLAND_DISPLAY=wayland-0
+if [ ! -O "${real_wayland}" ]; then
+  # On core systems mir-kiosk may also need to create the host XDG_RUNTIME_DIR
+  if [ ! -O "${real_xdg_runtime_dir}" ]; then
+    echo waiting for host XDG_RUNTIME_DIR...
+    until [ -O "${real_xdg_runtime_dir}" ]
+    do
+      inotifywait --event create $(dirname "${real_xdg_runtime_dir}") || sleep 4
+    done
+  fi
+
+  echo waiting for Wayland socket...
+  until [ -O "${real_wayland}" ]
+  do
+    inotifywait --event create $(dirname "${real_wayland}") || sleep 4
+  done
+
+  echo ...waiting done
 fi
-
-real_wayland=$(dirname "$XDG_RUNTIME_DIR")/${WAYLAND_DISPLAY}
-while [ ! -O "${real_wayland}" ]; do echo waiting for Wayland socket; sleep 4; done
-
-ln -sf "${real_wayland}" "$XDG_RUNTIME_DIR"
 
 exec "$@"
